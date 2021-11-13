c926f23d0611e7defe1e937c2ddf81884538b167
Mikhail Khlystunov <es.moll.89@gmail.com>
2021-11-14 01:17:39 +0700

    '0.1'

---
+++ database.js
@@ -0,0 +1,189 @@
+var db = {
+    "dog": {
+        "father": {
+            "name": "Пёс",
+            "img": "./img/dog_f.jpg",
+            "audio": "./audio/dog_f.mp3"
+        },
+        "mouther": {
+            "name": "Собака",
+            "img": "./img/dog_m.jpg",
+            "audio": "./audio/dog_m.mp3"
+        },
+        "child": {
+            "name": "Щенок",
+            "img": "./img/dog_c.jpg",
+            "audio": "./audio/dog_c.mp3"
+        }
+    },
+    "cat": {
+        "father": {
+            "name": "Кот",
+            "img": "./img/cat_f.jpg",
+            "audio": "./audio/cat_f.mp3"
+        },
+        "mouther": {
+            "name": "Кошка",
+            "img": "./img/cat_m.jpg",
+            "audio": "./audio/cat_m.mp3"
+        },
+        "child": {
+            "name": "Котёнок",
+            "img": "./img/cat_c.jpg",
+            "audio": "./audio/cat_c.mp3"
+        }
+    },
+    "chi": {
+        "father": {
+            "name": "Петух",
+            "img": "./img/chi_f.jpg",
+            "audio": "./audio/chi_f.mp3"
+        },
+        "mouther": {
+            "name": "Курица",
+            "img": "./img/chi_m.jpg",
+            "audio": "./audio/chi_m.mp3"
+        },
+        "child": {
+            "name": "Цыплёнок",
+            "img": "./img/chi_c.jpg",
+            "audio": "./audio/chi_c.mp3"
+        }
+    },
+    "goose": {
+        "father": {
+            "name": "Гусь",
+            "img": "./img/goose_f.jpg",
+            "audio": "./audio/goose_f.mp3"
+        },
+        "mouther": {
+            "name": "Гусыня",
+            "img": "./img/goose_m.jpg",
+            "audio": "./audio/goose_m.mp3"
+        },
+        "child": {
+            "name": "Гусёнок",
+            "img": "./img/goose_c.jpg",
+            "audio": "./audio/goose_c.mp3"
+        }
+    },
+    "duck": {
+        "father": {
+            "name": "Селезень",
+            "img": "./img/duck_f.jpg",
+            "audio": "./audio/duck_f.mp3"
+        },
+        "mouther": {
+            "name": "Утка",
+            "img": "./img/duck_m.jpg",
+            "audio": "./audio/duck_m.mp3"
+        },
+        "child": {
+            "name": "Утёнок",
+            "img": "./img/duck_c.jpg",
+            "audio": "./audio/duck_c.mp3"
+        }
+    },
+    "horse": {
+        "father": {
+            "name": "Конь",
+            "img": "./img/horse_f.jpg",
+            "audio": "./audio/horse_f.mp3"
+        },
+        "mouther": {
+            "name": "Лошадь",
+            "img": "./img/horse_m.jpg",
+            "audio": "./audio/horse_m.mp3"
+        },
+        "child": {
+            "name": "Жеребёнок",
+            "img": "./img/horse_c.jpg",
+            "audio": "./audio/horse_c.mp3"
+        }
+    },
+    "sheep": {
+        "father": {
+            "name": "Баран",
+            "img": "./img/sheep_f.jpg",
+            "audio": "./audio/sheep_f.mp3"
+        },
+        "mouther": {
+            "name": "Овца",
+            "img": "./img/sheep_m.jpg",
+            "audio": "./audio/sheep_m.mp3"
+        },
+        "child": {
+            "name": "Ягнёнок",
+            "img": "./img/sheep_c.jpg",
+            "audio": "./audio/sheep_c.mp3"
+        }
+    },
+    "goat": {
+        "father": {
+            "name": "Козёл",
+            "img": "./img/goat_f.jpg",
+            "audio": "./audio/goat_f.mp3"
+        },
+        "mouther": {
+            "name": "Коза",
+            "img": "./img/goat_m.jpg",
+            "audio": "./audio/goat_m.mp3"
+        },
+        "child": {
+            "name": "Козлёнок",
+            "img": "./img/goat_c.jpg",
+            "audio": "./audio/goat_c.mp3"
+        }
+    },
+    "cow": {
+        "father": {
+            "name": "Бык",
+            "img": "./img/cow_f.jpg",
+            "audio": "./audio/cow_f.mp3"
+        },
+        "mouther": {
+            "name": "Корова",
+            "img": "./img/cow_m.jpg",
+            "audio": "./audio/cow_m.mp3"
+        },
+        "child": {
+            "name": "Телёнок",
+            "img": "./img/cow_c.jpg",
+            "audio": "./audio/cow_c.mp3"
+        }
+    },
+    "pig": {
+        "father": {
+            "name": "Кабан",
+            "img": "./img/pig_f.jpg",
+            "audio": "./audio/pig_f.mp3"
+        },
+        "mouther": {
+            "name": "Свинья",
+            "img": "./img/pig_m.jpg",
+            "audio": "./audio/pig_m.mp3"
+        },
+        "child": {
+            "name": "Поросёнок",
+            "img": "./img/pig_c.jpg",
+            "audio": "./audio/pig_c.mp3"
+        }
+    },
+    "rabbit": {
+        "father": {
+            "name": "Кролик",
+            "img": "./img/rabbit_f.jpg",
+            "audio": "./audio/rabbit_f.mp3"
+        },
+        "mouther": {
+            "name": "Крольчиха",
+            "img": "./img/rabbit_m.jpg",
+            "audio": "./audio/rabbit_m.mp3"
+        },
+        "child": {
+            "name": "Крольчонок",
+            "img": "./img/rabbit_c.jpg",
+            "audio": "./audio/rabbit_c.mp3"
+        }
+    }
+}
